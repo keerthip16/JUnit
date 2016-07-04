764c3d9f46e385a3b685843a6bcdbfa41f30d3be
Darren Nguyen <dunghoangnguyen.math@gmail.com>
2016-07-04 17:29:39 +0700

    -- add two test classes

---
+++ src/java8/intro/test/OperationImplTest.java
@@ -0,0 +1,29 @@
+package java8.intro.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OperationImplTest {
+
+    @Test
+    public void testAdd() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testMultiply() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGCD() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSimplify() {
+        fail("Not yet implemented");
+    }
+
+}
